e6849a78dda99a71cce23aaa7296c00cac6b6bb3
Ekky Mulialasardi <ekky.mulialasardi@gmail.com>
2022-10-02 17:58:08 +0700

    fix news article

--- pages/news/[slug].jsx
+++ pages/news/[slug].jsx
@@ -5,7 +5,7 @@ import Fmita from "../../components/Sections/Fmita/fmita.component";
 const NewsDetails = () => {
     return (
         <Layout>
-            <div className="pt-12 md:pt-1 lg:px-32 lg:pt-0 lg:py-12">
+            <div className="pt-12 md:pt-1 lg:px-32 lg:pt-0 lg:py-12 overflow-x-hidden">
                 <NewsArticle/>
                 <Fmita ClassName="px-4 py-8 mx-2"/>
             </div>
