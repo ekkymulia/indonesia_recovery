a12f3bc2661a0d82c0a586d0d462b174cd043a28
Ekky Mulialasardi <ekky.mulialasardi@gmail.com>
2022-10-02 22:18:33 +0700

    adding sitemaps news article 2

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
     siteUrl: 'https://indonesia-recovery.vercel.app',
-    generateRobotsTxt: true, // (optional)
-    // ...other options
+    generateRobotsTxt: true, 
+    robotsTxtOptions:{
+      additionalSitemaps:[
+        'https://indonesia-recovery.vercel.app/sitemap.xml',
+        'https://indonesia-recovery.vercel.app/article-sitemap.xml'
+      ]
+    }
   }
