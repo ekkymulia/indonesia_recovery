843eb95d9643b58a9011ef0d16e80ae05ed3c35f
Ekky Mulialasardi <ekky.mulialasardi@gmail.com>
2022-10-02 17:49:25 +0700

    fix refix

--- pages/news/[slug].jsx
+++ pages/news/[slug].jsx
@@ -7,7 +7,7 @@ const NewsDetails = () => {
         <Layout>
             <div className="pt-12 md:pt-1 lg:px-32 lg:pt-0 lg:py-12">
                 <NewsArticle/>
-                <Fmita ClassName="px-4 py-8"/>
+                <Fmita ClassName="px-4 py-8 mx-4"/>
             </div>
         </Layout>
     );
