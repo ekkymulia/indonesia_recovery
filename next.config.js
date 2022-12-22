a077f974894aeca91886e331aa3a1b86f849f80d
Ekky Mulia Lasardi <86998473+ekkymulia@users.noreply.github.com>
2022-12-22 17:00:39 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,6 +2,13 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+
+  source: "/api/:path*",
+  headers: [
+    { key: "Access-Control-Allow-Credentials", value: "true" },
+    { key: "Access-Control-Allow-Origin", value: "*" },
+    { key: "Access-Control-Allow-Methods", value: "GET" },
+  ]
 }
 
 module.exports = nextConfig
